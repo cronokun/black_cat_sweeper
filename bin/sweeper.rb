1af39ec4e4f1cfd3e2a97bd79a7baf71c290bb87
cronokun <crono.neko@gmail.com>
2014-07-01 13:45:54 +0300

    Refactor main executable file

--- bin/sweeper.rb
+++ bin/sweeper.rb
@@ -18,33 +18,50 @@ INPUT_FILE = 'data/Revised Listing_25k.csv' # 'data/input_data.csv'
 VALID_FILE = 'data/valid_data.csv'
 DUPLICATIONS_FILE = 'data/duplications_data.csv'
 RUBBISH_FILE = 'data/rubbish_data.csv'
-REMOVED_ROWS_FILE = 'data/removed_rows.csv'
+EMPTY_ROWS_FILE = 'data/removed_rows.csv'
+
+# Setup logger
+@logger = Logger.new(STDOUT)
+@logger.level = Logger::INFO
 
 def write_csv_file(file, data)
   CSV.open(file, 'wb') do |csv|
-    csv << HEADER
+    csv << @header
     data.each { |record| csv << record.to_csv }
   end
 end
 
-logger = Logger.new(STDOUT)
-logger.level = Logger::INFO
+def sweep(message, file)
+  raise ArgumentError, 'Block must be provided!' unless block_given?
+  @logger.info message
+  invalid_data = yield
 
-# Read data from CSV file
-input_data = CSV.read(INPUT_FILE)
-logger.info "#{input_data.size} lines read."
-HEADER = input_data.shift
+  if invalid_data.empty?
+    @logger.info 'Nothing found.'
+  else
+    @logger.info "#{invalid_data.size} #{invalid_data.size == 1 ? 'record' : 'records'} found!"
+    write_csv_file(file, invalid_data)
+  end
+end
 
-input_data.map! { |row| Sweeper::Record.create_from_csv(row) }
 
-# Removing empty rows
-logger.info "Looking for empty rows..."
-empty = Sweeper::Empty.new(input_data).remove!
-logger.info "#{empty.size} records found!"
-write_csv_file(REMOVED_ROWS_FILE, empty)
+if $0 == __FILE__
 
-# Remove rubbish/test data
-logger.info "\nLooking for rubbish data..."
-rubbish = Sweeper::Rubbish.new(input_data).remove!
-logger.info "#{rubbish.size} records found!"
-write_csv_file(RUBBISH_FILE, rubbish)
+  # Read data from CSV file
+  input_data = CSV.read(INPUT_FILE)
+  @logger.info "#{input_data.size} lines read."
+  @header = input_data.shift
+
+  input_data.map! { |row| Sweeper::Record.create_from_csv(row) }
+
+  # Removing empty rows
+  sweep 'Looking for empty rows', EMPTY_ROWS_FILE do
+    Sweeper::Empty.new(input_data).remove!
+  end
+
+  # Remove rubbish/test data
+  sweep 'Looking for test/rubbish data', RUBBISH_FILE do
+    Sweeper::Rubbish.new(input_data).remove!
+  end
+
+end
