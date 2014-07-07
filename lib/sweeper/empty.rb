33f5e91d97121c7d7fdef9f8565cc2d5f9a637e5
melnikaite <melnikaite.melnikaite@gmail.com>
2014-07-07 16:20:53 +0300

    remove with only title and phone

--- lib/sweeper/empty.rb
+++ lib/sweeper/empty.rb
@@ -3,14 +3,24 @@ module Sweeper
     private
 
     def reject?(record)
-        empty_address?(record) &&
+      puts record.inspect if only_title_and_phone?(record)
+        only_title_and_phone?(record) ||
+        (empty_address?(record) &&
           record.email.nil? &&
           record.url.nil? &&
-          record.phone.nil?
+          record.phone.nil?)
     end
 
     def empty_address?(record)
       record.combined_address.join.strip.empty?
+    end
+
+    def only_title_and_phone?(record)
+      !record.title.nil? &&
+        !record.phone.nil? &&
+        record.url.nil? &&
+        record.email.nil? &&
+        empty_address?(record)
     end
   end
 end
